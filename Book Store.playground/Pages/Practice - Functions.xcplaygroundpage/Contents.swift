3142a4c4f6c310e5e8c9a872bb0dcb0b1a32b832
sherry40735 <sherry40735@gmail.com>
2016-10-24 22:36:06 +0800

    assign1_finish
    I'm 105462005.

---
+++ Book Store.playground/Pages/Practice - Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,91 @@
+/*:
+
+ # Your shopping cart
+
+ Now, it's your turn to create your shopping list.
+ 
+ Assume that you want to buy following books:
+
+ * "Digital Fortress" by "Dan Brown", $9.99
+ * "Angels & Demons" by "Dan Brown", $17.00
+ * "The Da Vinci Code" by "Dan Brown", $9.99
+ * "Deception Point" by "Dan Brown", $17.00
+ * "Harry Potter and the Goblet of Fire" by "J.K. Rowling", $12.99
+ * "Harry Potter and the Half-Blood Prince" by "J.K. Rowling", $12.99
+ * "Harry Potter and the Deathly Hallows" by "J.K. Rowling", $14.99
+ * "旅行與讀書" by "詹宏志", $12.00
+ * "國宴與家宴" by "王宣一", $7.99
+
+ Then, let's create a book store first:
+ 
+ */
+
+var bookStore = BookStore()
+
+/*:
+ 
+ Now start to feed data to the book store.
+ 
+ You have to prepare following functions:
+ 1. A function which returns the name of authors in a set or a list. 
+    _Note, you have to remove duplicated authors._
+ 2. A function which returns the totoal price of books to purchase
+ 3. A function which returns the number of books to buy
+ 4. A function which returns a book with its title, author, and price by a given index.
+    If the index is out of bound, return `nil`.
+ 
+ */
+
+let books: [[String: String]] = [
+    ["title": "Digital Fortress", "author": "Dan Brown", "price": "9.99"],
+    ["title": "Angels & Demons", "author": "Dan Brown", "price": "17.00"],
+    ["title": "The Da Vinci Code", "author": "Dan Brown", "price": "9.99"],
+    ["title": "Deception Point", "author": "Dan Brown", "price": "17.00"],
+    ["title": "Harry Potter and the Goblet of Fire", "author": "J.K. Rowling", "price": "12.99"],
+    ["title": "Harry Potter and the Half-Blood Prince", "author": "J.K. Rowling", "price": "12.99"],
+    ["title": "Harry Potter and the Deathly Hallows", "author": "J.K. Rowling", "price": "14.99"],
+    ["title": "旅行與讀書", "author": "詹宏志", "price": "12.00"],
+    ["title": "國宴與家宴", "author": "王宣一", "price": "7.99"]
+]
+
+func distinctAuthors() -> Set<String> {
+    var resultOfAuthor = Set<String>()
+    for book in books{
+        let author = book["author"]!
+        resultOfAuthor.insert(author)
+    }
+    return resultOfAuthor
+}
+bookStore.setDataSource(authorsGetter: distinctAuthors)
+
+func totalBookPrice() -> Double {
+    var resultOfPrice = 0.0
+    for book in books{
+        let bookPrice = Double(book["price"]!)
+        resultOfPrice = resultOfPrice + bookPrice!
+    }
+    return resultOfPrice
+}
+bookStore.setDataSource(priceCalculator: totalBookPrice)
+
+func getBook(at index: Int) -> Book? {
+    
+    if index > books.count - 1 || index < 0{
+        return nil
+    }
+    let book = books[index]
+    return (book["title"]!, book["author"]!, Double(book["price"]!)!)
+}
+
+
+bookStore.setDataSource(bookGetter: getBook(at:))
+/*:
+
+ Finally, let's show the book store shopping cart:
+
+ */
+
+bookStore.showInPlayground()
+
+//: ---
+//: [<- Previous](@previous) | [Next ->](@next)
